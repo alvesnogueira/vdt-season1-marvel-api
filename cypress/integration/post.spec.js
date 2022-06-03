41a57a4cad7b0da88eadac066376d2c51390aa88
Israel Alves <israelalvesdesign@gmail.com>
2022-06-02 23:09:26 -0300

    ajustments

--- cypress/integration/post.spec.js
+++ cypress/integration/post.spec.js
@@ -1,16 +1,45 @@
 describe('POST /characters', function(){
+    before(function(){
+        cy.request({
+            method: 'POST',
+            url: '/sessions',
+            body:{
+                email: 'israelalvesdesign@gmail.com',
+                password: 'guerra13'
+            }
+        }).then(function(response){
+            expect(response.status).eql(200)
+            cy.log(response.body.token)
+            Cypress.env('token', response.body.token)
+        })
+
+        cy.request({
+            method:'DELETE',
+            url: 'back2thepast/'+Cypress.env('id')
+        }).then(function(response){
+            expect(response.status).to.eql(200)
+        })
+    })
 
     it('shoutld create one hero', function(){
-        const hero = {
-            name: 'Charles Xavier',
-            alias: 'Professor X',
-            team: ['x-men', 'iluminatis'],
+
+        
+        let hero = {
+            name: 'Wanda Maximoff',
+            alias: 'Feiticeira Escarlate',
+            team: ['Vingadores'],
             active: 'true'
         }
 
-        cy.request(
+        cy.request({
             method: 'POST',
-            
-        )
+            url: '/characters',
+            body: hero,
+            headers:{
+                Authorization: Cypress.env('token')
+            }   
+        }).then(function(response){
+            expect(response.status).to.eql(201);
+        })
     })
 })
